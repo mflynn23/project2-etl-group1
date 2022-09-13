b34592ee664837b14f0d9050bc6c1773e27b9c79
apfleegor <apfleegor@oxy.edu>
2022-09-13 19:09:38 -0400

    revision

--- Schema_cars_db.sql
+++ Schema_cars_db.sql
@@ -7,10 +7,11 @@ CREATE TABLE "cars" ( "id" INT NOT NULL
                     , "power" VARCHAR   NOT NULL 
                     , "average_fuel_consumption" INT   NOT NULL
                     , "co2" INT   NOT NULL
-                    , "weight" INT   NOT NULL
+                    , "weight" VARCHAR   NOT NULL
                     , "material" VARCHAR   NOT NULL 
                     , "traction_type" VARCHAR   NOT NULL
 					, CONSTRAINT "pk_id" PRIMARY KEY (
         				"id"
      )
 );
+
